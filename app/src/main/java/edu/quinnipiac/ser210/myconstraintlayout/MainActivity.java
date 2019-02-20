0eec446e4cb85762678793d0ed1e5ba4d868b734
Brian Carballo <brian.carballo@quinnipiac.edu>
2019-02-20 16:39:50 -0500

    Final Demo

---
+++ app/src/main/java/edu/quinnipiac/ser210/myconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.myconstraintlayout;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
